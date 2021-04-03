5b0fbc07c6376fb54227f2c0e5b88c911cf5712a
Dlamba-Droid <67288662+Dlamba-Droid@users.noreply.github.com>
2021-04-03 14:14:44 +0100

    Add files via upload

---
+++ CSC Assignment SxN 2.py
@@ -0,0 +1,26 @@
+print("Daniel Alamba,","Computer Science Department")
+
+scare="Boo!"
+print(scare)
+
+a = 4
+b = 3
+c = a+b
+print(c)
+
+d = 12/5
+print(d)
+
+e = 3+3
+print(e)
+
+f = 8%3
+print(f)
+
+
+g = 2*4
+h = 4+2
+i = g*h
+
+print(i)
+
